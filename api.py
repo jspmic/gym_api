de07719615d24fddd32468e7423fd3a77e296eb6
Jaspe Michael <micaeljaspe@gmail.com>
2025-03-09 19:11:50 +0200

    feat: added app configuration to point to the rightSQLALCHEMY_DATABASE_URI

--- api.py
+++ api.py
@@ -1,7 +1,11 @@
 from flask import Flask
-from init import api
+from init import api, DB
 from models import db, Sets, Exercise
 
 app = Flask(__name__)
+
+# app configuration
+app.config["SQLALCHEMY_DATABASE_URI"] = DB
+
 api.init_app(app)
 db.init_app(app)
